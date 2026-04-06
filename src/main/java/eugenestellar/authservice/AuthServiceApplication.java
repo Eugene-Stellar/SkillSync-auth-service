cfbde77b54ee83852ab31bea39ea74b4b1c31856
Eugene <bielichenko777eugene@gmail.com>
2026-04-06 19:34:03 +0200

    Exclude UserDetailsServiceAutoConfiguration to prevent default password setup in Spring Security

--- src/main/java/eugenestellar/authservice/AuthServiceApplication.java
+++ src/main/java/eugenestellar/authservice/AuthServiceApplication.java
@@ -2,12 +2,11 @@ package eugenestellar.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class}) // to shun a default password from SpringSecurity
 public class AuthServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServiceApplication.class, args);
 	}
-
 }
