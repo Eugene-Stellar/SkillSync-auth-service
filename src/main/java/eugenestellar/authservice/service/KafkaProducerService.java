a3ed10201d8147cb4ea98c851f3ecfddddab39d8
Eugene <bielichenko777eugene@gmail.com>
2026-04-06 19:35:04 +0200

    Add KafkaProducerService for sending user registration events to Kafka

---
+++ src/main/java/eugenestellar/authservice/service/KafkaProducerService.java
@@ -0,0 +1,36 @@
+package eugenestellar.authservice.service;
+
+import eugenestellar.authservice.model.dto.UserRegisteredEventDto;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class KafkaProducerService {
+
+  private final KafkaTemplate<String, UserRegisteredEventDto> kafkaTemplate;
+
+  private static final String TOPIC = "topic-registration";
+
+  public KafkaProducerService(KafkaTemplate<String, UserRegisteredEventDto> kafkaTemplate) {
+    this.kafkaTemplate = kafkaTemplate;
+  }
+
+  public void sendRegistrationEvent(UserRegisteredEventDto event) {
+    String key = String.valueOf(event.userId());
+
+    CompletableFuture<SendResult<String, UserRegisteredEventDto>> future =
+        kafkaTemplate.send(TOPIC, key, event);
+
+    future.whenComplete((result, ex) -> {
+      if (ex == null) {
+        System.out.println("Event was sent to Kafka!");
+      } else {
+        System.err.println("Error occurred during sending to Kafka: " + ex.getMessage());
+      }
+    });
+  }
+
+}
