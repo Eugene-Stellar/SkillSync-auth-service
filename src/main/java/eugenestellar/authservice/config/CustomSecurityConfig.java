56f548c08cae4c0e2bb36dd577785cd9accbecf1
Eugene <bielichenko777eugene@gmail.com>
2026-04-06 19:34:22 +0200

    Add CustomSecurityConfig for JWT-based security configuration and CORS setup

---
+++ src/main/java/eugenestellar/authservice/config/CustomSecurityConfig.java
@@ -0,0 +1,70 @@
+package eugenestellar.authservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CustomSecurityConfig {
+
+  private final JwtFilter jwtFilter;
+  private final String frontendUrl;
+  private final GatewaySecretFilter gatewaySecretFilter;
+
+  public CustomSecurityConfig(JwtFilter jwtFilter,
+                              @Value("${API_GATEWAY_URL}") String frontendUrl,
+                              GatewaySecretFilter gatewaySecretFilter) {
+    this.jwtFilter = jwtFilter;
+    this.frontendUrl = frontendUrl;
+    this.gatewaySecretFilter = gatewaySecretFilter;
+  }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
+    http
+        .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+        // disable, because it's for session rather than JWT
+        .csrf(csrf -> csrf.disable())
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/auth/**", "/error", "/.well-known/**").permitAll()
+            .anyRequest().authenticated())
+        .exceptionHandling(ex ->
+            ex.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+        .addFilterBefore(gatewaySecretFilter, UsernamePasswordAuthenticationFilter.class)
+        .addFilterAfter(jwtFilter, GatewaySecretFilter.class);
+    return http.build();
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOriginPatterns(List.of(frontendUrl));
+    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
+    configuration.setAllowedHeaders(List.of("*"));
+    configuration.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration); // apply cors config to all paths
+    return source;
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder(10); // tradeoff for performance (not 11)
+  }
+}
