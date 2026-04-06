c0e42107210f021b3b1b569ebd58f09961580a13
Eugene <bielichenko777eugene@gmail.com>
2026-04-06 19:36:09 +0200

    Add ProfileCreationException for handling profile creation errors

---
+++ src/main/java/eugenestellar/authservice/exception/ProfileCreationException.java
@@ -0,0 +1,7 @@
+package eugenestellar.authservice.exception;
+
+public class ProfileCreationException extends RuntimeException {
+  public ProfileCreationException(String message) {
+    super(message);
+  }
+}
