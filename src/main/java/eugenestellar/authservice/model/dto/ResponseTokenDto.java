974c4b33dfa4fc97fa7601388e0126082c47b6b7
Eugene <bielichenko777eugene@gmail.com>
2026-04-06 19:36:18 +0200

    Add ResponseTokenDto for token response handling

---
+++ src/main/java/eugenestellar/authservice/model/dto/ResponseTokenDto.java
@@ -0,0 +1,14 @@
+package eugenestellar.authservice.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseTokenDto {
+  private String token;
+}
