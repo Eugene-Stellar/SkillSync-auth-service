4099116a842289ab69beca207fb1cb475165d215
Eugene <bielichenko777eugene@gmail.com>
2026-04-19 19:39:32 +0200

    Remove unused LogFactory import from GatewaySecretFilter

--- src/main/java/eugenestellar/authservice/config/GatewaySecretFilter.java
+++ src/main/java/eugenestellar/authservice/config/GatewaySecretFilter.java
@@ -4,7 +4,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.apache.juli.logging.LogFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
